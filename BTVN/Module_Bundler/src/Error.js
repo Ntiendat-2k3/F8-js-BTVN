584c5dd5546980b8353e0ac05e33bb14a9ee2910
Ntiendat-2k3 <107376328+Ntiendat-2k3@users.noreply.github.com>
2023-10-29 16:19:40 +0700

    update code

--- BTVN/Module_Bundler/src/Error.js
+++ BTVN/Module_Bundler/src/Error.js
@@ -2,13 +2,15 @@ import image from "./Assets/Image/404-error.jpg";
 import "./Assets/Error.scss";
 
 export const Error = () => {
-        document.body.classList.add("error-state");
-
-        document.querySelector("#app").innerHTML = `
-            <div class="error-page">
-                <img src="${image}" alt="404-Error" />
-                <h2>Oops! Trang bạn đang tìm kiếm không tồn tại.</h2>
-                <p>Xin lỗi vì sự bất tiện này. Hãy thử lại hoặc quay lại trang chính.</p>
-            </div>
-        `;
+     document.body.classList.add("error-state");
+     const overlay = document.createElement("div");
+     overlay.className = "error-overlay";
+     overlay.innerHTML = `
+          <div class="error-page">
+               <img src="${image}" alt="404-Error" />
+               <h2>Oops! Trang bạn đang tìm kiếm không tồn tại.</h2>
+               <p>Xin lỗi vì sự bất tiện này. Hãy thử lại hoặc quay lại trang chính.</p>
+          </div>
+     `;
+     document.body.appendChild(overlay);
 };
